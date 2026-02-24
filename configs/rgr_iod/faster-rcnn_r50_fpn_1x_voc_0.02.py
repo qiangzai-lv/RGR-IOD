e27720dee5f8d19518ff11762455a7425da1a0a3
lvxueqiang <3486105329@qq.com>
2026-02-24 22:06:45 +0800

    sd1.5-pretrain

--- configs/rgr_iod/faster-rcnn_r50_fpn_1x_voc_0.02.py
+++ configs/rgr_iod/faster-rcnn_r50_fpn_1x_voc_0.02.py
@@ -1,9 +1,15 @@
 _base_ = [
-    '../_base_/datasets/coco_detection.py', '../_base_/default_runtime.py'
+    '../_base_/datasets/voc07.py',
+    '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
 ]
+
+num_classes = 20
+
+backbone_checkpoint = 'https://download.pytorch.org/models/resnet50-11ad3fa6.pth'
+
 # model settings
 model = dict(
-    type='GridRCNN',
+    type='FasterRCNN',
     data_preprocessor=dict(
         type='DetDataPreprocessor',
         mean=[123.675, 116.28, 103.53],
@@ -19,7 +25,7 @@ model = dict(
         norm_cfg=dict(type='BN', requires_grad=True),
         norm_eval=True,
         style='pytorch',
-        init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet50')),
+        init_cfg=dict(type='Pretrained', checkpoint=backbone_checkpoint)),
     neck=dict(
         type='FPN',
         in_channels=[256, 512, 1024, 2048],
@@ -40,9 +46,9 @@ model = dict(
             target_stds=[1.0, 1.0, 1.0, 1.0]),
         loss_cls=dict(
             type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0),
-        loss_bbox=dict(type='SmoothL1Loss', beta=1.0 / 9.0, loss_weight=1.0)),
+        loss_bbox=dict(type='L1Loss', loss_weight=1.0)),
     roi_head=dict(
-        type='GridRoIHead',
+        type='StandardRoIHead',
         bbox_roi_extractor=dict(
             type='SingleRoIExtractor',
             roi_layer=dict(type='RoIAlign', output_size=7, sampling_ratio=0),
@@ -50,30 +56,18 @@ model = dict(
             featmap_strides=[4, 8, 16, 32]),
         bbox_head=dict(
             type='Shared2FCBBoxHead',
-            with_reg=False,
             in_channels=256,
             fc_out_channels=1024,
             roi_feat_size=7,
-            num_classes=80,
+            num_classes=num_classes,
             bbox_coder=dict(
                 type='DeltaXYWHBBoxCoder',
                 target_means=[0., 0., 0., 0.],
                 target_stds=[0.1, 0.1, 0.2, 0.2]),
-            reg_class_agnostic=False),
-        grid_roi_extractor=dict(
-            type='SingleRoIExtractor',
-            roi_layer=dict(type='RoIAlign', output_size=14, sampling_ratio=0),
-            out_channels=256,
-            featmap_strides=[4, 8, 16, 32]),
-        grid_head=dict(
-            type='GridHead',
-            grid_points=9,
-            num_convs=8,
-            in_channels=256,
-            point_feat_channels=64,
-            norm_cfg=dict(type='GN', num_groups=36),
-            loss_grid=dict(
-                type='CrossEntropyLoss', use_sigmoid=True, loss_weight=15))),
+            reg_class_agnostic=True,
+            loss_cls=dict(
+                type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0),
+            loss_bbox=dict(type='L1Loss', loss_weight=1.0))),
     # model training and testing settings
     train_cfg=dict(
         rpn=dict(
@@ -82,6 +76,7 @@ model = dict(
                 pos_iou_thr=0.7,
                 neg_iou_thr=0.3,
                 min_pos_iou=0.3,
+                match_low_quality=True,
                 ignore_iof_thr=-1),
             sampler=dict(
                 type='RandomSampler',
@@ -89,12 +84,12 @@ model = dict(
                 pos_fraction=0.5,
                 neg_pos_ub=-1,
                 add_gt_as_proposals=False),
-            allowed_border=0,
+            allowed_border=-1,
             pos_weight=-1,
             debug=False),
         rpn_proposal=dict(
             nms_pre=2000,
-            max_per_img=2000,
+            max_per_img=1000,
             nms=dict(type='nms', iou_threshold=0.7),
             min_bbox_size=0),
         rcnn=dict(
@@ -103,6 +98,7 @@ model = dict(
                 pos_iou_thr=0.5,
                 neg_iou_thr=0.5,
                 min_pos_iou=0.5,
+                match_low_quality=False,
                 ignore_iof_thr=-1),
             sampler=dict(
                 type='RandomSampler',
@@ -110,9 +106,7 @@ model = dict(
                 pos_fraction=0.25,
                 neg_pos_ub=-1,
                 add_gt_as_proposals=True),
-            pos_radius=1,
             pos_weight=-1,
-            max_num_grid=192,
             debug=False)),
     test_cfg=dict(
         rpn=dict(
@@ -121,40 +115,11 @@ model = dict(
             nms=dict(type='nms', iou_threshold=0.7),
             min_bbox_size=0),
         rcnn=dict(
-            score_thr=0.03,
-            nms=dict(type='nms', iou_threshold=0.3),
-            max_per_img=100)))
-# optimizer
+            score_thr=0.05,
+            nms=dict(type='nms', iou_threshold=0.5),
+            max_per_img=100)
+    ))
+
 optim_wrapper = dict(
     type='OptimWrapper',
-    optimizer=dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001))
-
-# training schedule
-max_epochs = 25
-train_cfg = dict(
-    type='EpochBasedTrainLoop', max_epochs=max_epochs, val_interval=1)
-val_cfg = dict(type='ValLoop')
-test_cfg = dict(type='TestLoop')
-
-# learning rate
-param_scheduler = [
-    dict(
-        type='LinearLR',
-        start_factor=1.0 / 80,
-        by_epoch=False,
-        begin=0,
-        end=3665),
-    dict(
-        type='MultiStepLR',
-        begin=0,
-        end=max_epochs,
-        by_epoch=True,
-        milestones=[17, 23],
-        gamma=0.1)
-]
-
-# Default setting for scaling LR automatically
-#   - `enable` means enable scaling LR automatically
-#       or not by default.
-#   - `base_batch_size` = (8 GPUs) x (2 samples per GPU).
-auto_scale_lr = dict(enable=False, base_batch_size=16)
+    optimizer=dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001))
